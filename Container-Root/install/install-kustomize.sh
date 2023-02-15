cbdff8485d69e34ec8b76496af4242ac14878149
Alex Iankoulski <iankouls@amazon.com>
2023-02-14 21:48:12 -0800

    Pin kustomize version to 3.2.0

--- Container-Root/install/install-kustomize.sh
+++ Container-Root/install/install-kustomize.sh
@@ -3,7 +3,11 @@
 echo ""
 echo "Installing kustomize ..."
 
-curl -s "https://raw.githubusercontent.com/kubernetes-sigs/kustomize/master/hack/install_kustomize.sh"  | bash
+#curl -s "https://raw.githubusercontent.com/kubernetes-sigs/kustomize/master/hack/install_kustomize.sh"  | bash
+curl -o kustomize -L https://github.com/kubernetes-sigs/kustomize/releases/download/v3.2.0/kustomize_3.2.0_linux_amd64
+chmod +x kustomize
+mv ./kustomize /usr/local/bin
 
-mv /kustomize /usr/local/bin
+#mv /kustomize /usr/local/bin
 
+kustomize version
